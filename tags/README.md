2abe2076644ff9c237e9d36ddf704772e2a32057
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-11-07 15:35:37 +0545

    feat: create tag and taggedItem model with generic relation and register models

--- tags/README.md
+++ tags/README.md
@@ -3,3 +3,13 @@
 ## Database Entities in Tag App
 1. Tag
 2. TaggedItem
+
+### Tag Attributes
+1. label
+
+
+### TagItems Attributes
+1. tag
+2. content_type
+3. object_id
+4. content_object
--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.label
+
+class TaggedItem(models.Model):
+    # what tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    '''
+    we don't want to make this model dependent upon shop.product
+    So we use generic objects and relation ship from django.contrib.contenttypes.models
+    We need two info to determine the object we want to tag it
+    1. Type (product, articles, blog, news, videos, etc) ---> content_type 
+    2. ID ---> object id
+    3. generic foreigh relation --> content_object
+    Its limitation is it works only if the id is the integer
+    '''
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)#type of object
+    object_id = models.PositiveIntegerField()# id of the object
+    content_object = GenericForeignKey()
