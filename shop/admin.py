391e5176421c9b458c57ab29bf14d5cb97023e48
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-11-28 08:37:43 +0545

    feat: add product image inline to product admin     - Create a tabular inline class of ProductImage Mode     - Create a readonly field thumbnail     - Create a method thumbnail that returns the actual thumbnail of the image with the help of format_html     - Create a styles.css file in shop/static/shop/css     - Create a media class inside ProductAdmin class     - Define a css file path for admin page

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import *
+
 # Register your models here.
 
-admin.site.register(Product)
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra: int = 0
+    readonly_fields = ['thumbnail']
+
+    def thumbnail(self, instance):
+        if instance.image.name != "":
+            return format_html(f'<img src="{instance.image.url}" class="thumbnail" />')
+        return ""
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+
+    class Media:
+        css = {
+            'all':['shop/css/styles.css']
+        }
+
+
 admin.site.register(Order)
 admin.site.register(Customer)
 admin.site.register(OrderItem)
