071e17d610de01a7d5818a87eef7e793d3bec7dd
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-11-28 07:27:38 +0545

    feat: create an image models that stores the images related to products

--- shop/models.py
+++ shop/models.py
@@ -40,6 +40,11 @@ class Product(models.Model):
         ordering = ['title']
 
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="store/product/images", default="default.jpg")
+
+
 # Customer Database Model
 class Customer(models.Model):
     class MEMBERSHIPS_CHOICES(models.TextChoices):
